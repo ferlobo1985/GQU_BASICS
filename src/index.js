13133b7de3cfa447a0a5aea0b4b9f223c8e012ac
Fernando <dev.teach.user.dummy@gmail.com>
2020-08-27 13:25:44 -0300

    06- arguements and arrays 2

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import axios from 'axios';
 // Scalar types
 // object types
 
+const db = 'http://localhost:3004';
+
 const server = new GraphQLServer({
     typeDefs:`
         type Query {
             agent(id:ID!): User!
-            agents: [User!]!
+            agents(name:String,age:Int): [User!]!
+            cars:[String!]!
+            msg(values:[String!]!):String
         }
 
         type User {
@@ -21,12 +25,24 @@ const server = new GraphQLServer({
     resolvers:{
         Query:{
             agent:async(parent,args,context,info)=>{
-                const response = await axios.get(`http://localhost:3004/users/${args.id}`);
+                const response = await axios.get(`${db}/users/${args.id}`);
                 return response.data
             },
-            agents:async()=>{
-                const response = await axios.get('http://localhost:3004/users');
+            agents:async(parent,args,context,info)=>{
+                const name = args.name != null ? `name=${args.name}`: '';
+                const age = args.age != null ? `age=${args.age}`:''
+
+                const response = await axios.get(`${db}/users?${name}&${age}`);
                 return response.data
+            },
+            cars:()=>{
+                return ['Ford',null,'Porsche']
+            },
+            msg:(parent,args,context,info)=>{
+                if(args.values.length === 0){
+                    return `Sorry, no values`
+                }
+                return `Hello ${args.values[0]}  ${args.values[1]}`
             }
         }
     }
