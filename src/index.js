d0d77e0562f0a9e08c165ceaaecf68c5da1df7b4
Fernando <dev.teach.user.dummy@gmail.com>
2020-08-27 10:55:49 -0300

    02- Scalar types

--- src/index.js
+++ src/index.js
@@ -1,15 +1,33 @@
 import { GraphQLServer } from 'graphql-yoga';
 
+// Scalar types
+
 const server = new GraphQLServer({
     typeDefs:`
         type Query {
-            start: String!
+           id: ID!
+           name: String!
+           age: Int
+           married: Boolean!
+           average: Float
         }
     `,
     resolvers:{
         Query:{
-            start(){
-                return 'We are starting'
+            id(){
+                return 1
+            },
+            name(){
+                return 'John'
+            },
+            age(){
+                return 50
+            },
+            married(){
+                return null
+            },
+            average(){
+                return  3.5
             }
         }
     }
