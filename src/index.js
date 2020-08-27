97fab54811051e3534bbb8c6184b4a06a33008a8
Fernando <dev.teach.user.dummy@gmail.com>
2020-08-27 11:23:35 -0300

    03 -- Other types

--- src/index.js
+++ src/index.js
@@ -1,34 +1,53 @@
 import { GraphQLServer } from 'graphql-yoga';
 
 // Scalar types
+// object types
 
 const server = new GraphQLServer({
     typeDefs:`
         type Query {
-           id: ID!
-           name: String!
-           age: Int
-           married: Boolean!
-           average: Float
+            agent: User!
+            agents: [User!]!
+        }
+
+        type User {
+            id: ID!
+            name: String!
+            age: Int
+            married: Boolean!
+            average: Float
         }
     `,
     resolvers:{
         Query:{
-            id(){
-                return 1
+            agent(){
+                return {
+                    id: 1,
+                    name:'Steve',
+                    age: null,
+                    married: true,
+                    average: 4.5,
+                }
             },
-            name(){
-                return 'John'
-            },
-            age(){
-                return 50
-            },
-            married(){
-                return null
-            },
-            average(){
-                return  3.5
+            agents(){
+                return [
+                    {
+                        id: 1,
+                        name:'Steve',
+                        age: null,
+                        married: true,
+                        average: 4.5,
+                    },
+                    {
+                        id: 1,
+                        name:'Francis',
+                        age: null,
+                        married: true,
+                        average: 4.5,
+                    }
+                ]
             }
+
         }
     }
 });
